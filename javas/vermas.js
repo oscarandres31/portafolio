1ba56aaa97caa03fa36089c71f3f6eef2130439e
oscarandres31 <oarrieta3@gmail.com>
2022-09-25 18:14:43 -0500

    primera prueba de mi portafolio

---
+++ javas/vermas.js
@@ -0,0 +1,40 @@
+function vermas(id) {
+    if (id == "mas") {
+        document.getElementById("desplegar").style.display = "block";
+        document.getElementById("mas").style.display = "none";
+    }
+    else {
+        document.getElementById("desplegar").style.display = "none";
+        document.getElementById("mas").style.display = "inline";
+    }
+}
+function vermas1(id) {
+    if (id == "mass") {
+        document.getElementById("desplegar2").style.display = "block";
+        document.getElementById("mass").style.display = "none";
+    }
+    else {
+        document.getElementById("desplegar2").style.display = "none";
+        document.getElementById("mass").style.display = "inline";
+    }
+}
+function vermas2(id) {
+    if (id == "mas-") {
+        document.getElementById("desplegar3").style.display = "block";
+        document.getElementById("mas-").style.display = "none";
+    }
+    else {
+        document.getElementById("desplegar3").style.display = "none";
+        document.getElementById("mas-").style.display = "inline";
+    }
+}
+function vermas3(id) {
+    if (id == "mas+") {
+        document.getElementById("desplegar4").style.display = "block";
+        document.getElementById("mas+").style.display = "none";
+    }
+    else {
+        document.getElementById("desplegar4").style.display = "none";
+        document.getElementById("mas+").style.display = "inline";
+    }
+}
